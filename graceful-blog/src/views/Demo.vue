43661692f6509fb0246c0c831ab3fa7efdad3361
cgx <codercgx@qq.com>
2021-01-31 14:53:35 +0800

    完善demo页面

--- graceful-blog/src/views/Demo.vue
+++ graceful-blog/src/views/Demo.vue
@@ -1,93 +1,176 @@
 <template>
   <div id="demo">
-    DEMO
+    <div class="wrap">
+      <el-card v-for="(item, index) in demoData">
+        <div class="card_item">
+          <div class="demo_img">
+            <img
+              @click="previewDemoFile(item)"
+              class="img_box"
+              v-if="item.files.length > 0"
+              :src="item.files[0].file_url"
+              alt=""
+            />
+            <img v-else src="../assets/logo.png" alt="" />
+          </div>
+          <div class="demo_desc">
+            <div class="demo_title" @click="previewDemo(item._id)">
+              <p>{{ item.title }}</p>
+            </div>
+            <div class="demo_description">
+              <span>{{ item.description }}</span>
+            </div>
+            <div class="demo_label">
+              <el-tag
+                v-for="tag in item.label"
+                :key="tag._id"
+                :type="labelTypes[Math.ceil(Math.random() * 5)]"
+                >{{ tag.name }}</el-tag
+              >
+            </div>
+            <div class="demo_create">发布于: {{ item.create_time }}</div>
+          </div>
+        </div>
+      </el-card>
+      <el-dialog title="预览"  width="50%" center  v-model="dialogVisible">
+        <el-carousel trigger="click" height="500px">
+          <el-carousel-item v-for="item in previewDemoFileData" :key="item.file_url">
+            <img :src="item.file_url" alt="">
+          </el-carousel-item>
+        </el-carousel>
+      </el-dialog>
+    </div>
   </div>
 </template>
 <script>
-export default {
-  name: "demo",
- 
-  
-};
+import { defineComponent, onMounted, ref } from "vue";
+import { demoApi } from "@/utils/http/request";
+import { useRouter } from "vue-router";
+export default defineComponent({
+  setup() {
+    const demoData = ref([]);
+    const router = useRouter();
+    const labelTypes = ref(["", "success", "wraning", "danger", "info"]);
+    const previewDemoFileData = ref([]);
+    const dialogVisible=ref(false)
+    const previewDemoFile = (item) => {
+      console.log(item);
+      dialogVisible.value=true
+      previewDemoFileData.value=item.files
+    };
+    onMounted(() => {
+      getDemoList();
+    });
+
+    const getDemoList = async () => {
+      const result = await demoApi.getDemoList();
+      console.log(result);
+      demoData.value = result.demoList;
+
+      // demoData.value = result.demoList.map(item=> ({
+      //   ...item,
+      //   files: item.files.length>0 ? item.files : [{
+      //     file_url: '../assets/logo.png'
+      //   }]
+      // }));
+    };
+    const previewDemo = (id) => {
+      router.push(`/demo/${id}`);
+    };
+
+    return {
+      demoData,
+      previewDemo,
+      labelTypes,
+      previewDemoFileData,
+      previewDemoFile,
+      dialogVisible
+    };
+  },
+});
 </script>
 <style lang="scss" scoped>
 #demo {
-  width: 90%;
-  margin: 0 auto;
-  display: flex;
-  flex-wrap: wrap;
-  position: relative;
-  padding: 3rem 0;
-  .demoItem {
-    width: 30%;
-    margin: 1rem;
-    // padding: 0.8rem;
-    height: 22rem;
-    background: rgba(34, 34, 34,.8);
-    border-radius: 0.5rem;
-    box-shadow:0 0 3px #333;
-    position: relative;
-    color: #f2f2f2;
-    position: relative;
-     &::before{
-        content: "";
-        display: none;
-        position: absolute;
-        z-index: 3;
-        top: 0;
-        left: 0;
+  .wrap {
+    display: flex;
+    align-items: center;
+    :deep() .el-card {
+      margin: 20px;
+      border-radius: 10px;
+      &:hover {
+        transform: scale(0.97);
+      }
+    }
+    padding: 10px 150px;
+    .card_item {
+      display: flex;
+      align-items: center;
+      .demo_img {
+        width: 100px;
+        height: 100px;
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 10px;
+        }
+        .img_box {
+          position: relative;
+          width: 100%;
+          height: 100%;
+          &:hover {
+            position: relative;
+            cursor: pointer;
+            // transform: scale(0.95);
+            &::after {
+              content: "查看";
+              color: #fff;
+              display: block;
+              position: absolute;
+              z-index: 999;
+              top: 0;
+              left: 0;
+              width: 100px;
+              height: 100px;
+              background-color: rgba(255, 255, 255, 0.1);
+            }
+          }
+        }
+      }
+      .demo_desc {
+        margin-left: 20px;
+        .demo_title {
+          &:hover {
+            cursor: pointer;
+          }
+          p {
+            font-size: 14px;
+            margin: 5px 0 5px 0;
+          }
+        }
+        .demo_description {
+          font-size: 13px;
+          color: #999;
+        }
+        .demo_label {
+          margin: 5px 0;
+          .el-tag {
+            margin-right: 5px;
+          }
+        }
+        .demo_create {
+          font-size: 13px;
+          color: #999;
+        }
+      }
+    }
+  }
+  :deep().el-dialog{
+    :deep().el-carousel__item{
+      img{
         width: 100%;
         height: 100%;
-        background: rgba(0,0,0,.3);
-      }
-      &:hover::before{
-        display: block;
-        transition: all 1s ease 100ms;
-      }
-    #video {
-      border-radius: 0.5rem;
-      position: relative;
-      width: 100%;
-      height: 66%;
-      outline: none;
-      z-index: 2;
-    }
-    footer {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      padding: 0.4rem;
-      p {
-        font-size: 12px;
-      }
-      button {
-        position: relative;
-        z-index: 5;
-        flex:1;
-      }
-      span {
-        color:lightgreen;
       }
     }
-  }
-  @media screen and(max-width: 955px) {
-    .demoItem {
-      width: 45%;
-      height: 18rem;
-    }
-  }
-  @media screen and(max-width: 568px) {
-    .demoItem {
-      width: 100%;
-      height: 25rem;
-    }
-  }
-  .page {
-    position: absolute;
-    bottom: -1rem;
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: 99;
   }
 }
 </style>
